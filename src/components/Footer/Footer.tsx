32dfb2a6bf0155867df71862eda603aba76d0b24
Maciej Walecki <maciejwalecki99@gmail.com>
2022-01-21 22:19:00 +0100

    Fixed footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,12 @@
 import {
     FooterContainer,
-    MediaIcons,
 } from './styles';
-import { Facebook } from '@styled-icons/bootstrap/Facebook';
-import { Linkedin } from '@styled-icons/bootstrap/Linkedin';
-import { Github } from '@styled-icons/bootstrap/Github';
 
 const Footer = () => {
     return(
         <FooterContainer>
-            <MediaIcons>
-                <a href="https://www.facebook.com/maciek.walecki.7" target="_blank"><Facebook /></a>
-                <a href="https://www.linkedin.com/in/maciej-walecki-51b76a1b7/" target="_blank"><Linkedin /></a>
-                <a href="https://github.com/maciekw129" target="_blank"><Github /></a>
-            </MediaIcons>
+            <h5>created and designed by Maciej Walecki</h5>
+            <h5>-</h5>
             <h5>©2021 Copyright - All rights Reserved</h5>
         </FooterContainer>
     )
